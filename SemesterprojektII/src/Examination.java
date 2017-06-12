89978255204e9d21a496cbabe44c5253364071ff
Louise Moesgaard <louise@moesgaard.dk>
2017-06-12 20:53:48 +0200

    videreudvikling af graf. Der skal fejlfindes, så graferne kan skrives på GUI

--- SemesterprojektII/src/Examination.java
+++ SemesterprojektII/src/Examination.java
@@ -4,12 +4,20 @@ import javax.swing.JFrame;
 public class Examination {
     
     public static void main (String[] args ){
+        DataAccess dao = new DataAccess();
+        
         JFrame frame = new JFrame();
-       // frame.add(new GUIPanel());
-       frame.add(new Graph());
+        GUIPanel gui = new GUIPanel();
+        Graph pulse = new Graph(dao.getPulse(), gui.jPanel3);
+        Graph ekg = new Graph(dao.getEKG(), gui.jPanel2);
+        gui.jPanel3.add(pulse);
+        gui.jPanel2.add(ekg);
+        
+        frame.add(gui);
+        
         frame.setTitle("Semesterprojekt EKG");
         //til når ny frame
-        frame.setSize(700,800);
+        frame.setSize(850,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.setResizable(false);
         //frame.pack();
--- SemesterprojektII/src/Graph.java
+++ SemesterprojektII/src/Graph.java
@@ -5,68 +5,133 @@ import javax.swing.*;
 
 public class Graph extends JPanel  {
   
-    private final int width = 800;
-    private final int heigth = 400;
     private final Color lineColor =  new Color(44, 102, 230, 180);
     private final Color pointColor = new Color (100, 100, 100, 180);
     private final Color gridColor = new Color (200, 200, 200, 200);
     private final static Stroke GRAPH_STROKE = new BasicStroke(2f);
     private final int pointWidth= 4;
+    private static final int BORDER_GAP = 30;
     private final int numberYDivisions = 9;
-    private ArrayList<Integer> EKGData, pulsdata = new ArrayList<Integer>();
+    private ArrayList<Integer> data = new ArrayList<Integer>();
     private final int padding = 25;
     private final int labelPadding = 25;
-    GUIPanel pan = new GUIPanel();
-    
-    int ekgwidth,pulswidth,ekgheight,pulsheight;
-    
-//    DataAccess da = new DataAccess();
-    ArrayList data ;
-    
-    public Graph(){
-       //pulsdata = da.getPulse();
-      // EKGData = da.getEKG();
+    private int width, height;
+  
+      
+    public Graph(ArrayList<Integer> data, JPanel pan){
+       this.data = data;
        
-       Dimension pulsdim = pan.jPanel3.getSize();
-       Dimension ekgdim = pan.jPanel2.getSize();
+       Dimension dim = pan.getSize();
        
-       ekgwidth = ekgdim.width;
-       pulswidth = pulsdim.width;
-       ekgheight = ekgdim.height;
-       pulsheight = pulsdim.height;
+     
+       this.width = dim.width;
+       this.height = dim.height;
+
      simulerData();
     }
-    
-    
-    
-    
-    
-@Override
-protected void paintComponent(Graphics paint) {
-    super.paintComponent(paint);
-    Graphics2D paint2D = (Graphics2D) paint;
-    paint2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    /*vi henter data ind hver gang vi repainter*/
-   
-//skaler x aksen
+       
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        super.paintComponent(g);
 
-paint.drawLine(0, 0, 600, 234);
+        double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (this.data.size() - 1);
+        double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMax() - getMin());
 
+        List<Point> graphPoints = new ArrayList<>();
+        for (int i = 0; i < this.data.size(); i++) {
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMax() - data.get(i)) * yScale + padding);
+            graphPoints.add(new Point(x1, y1));
+        }
 
+         /*tegner hvid baggrund*/
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
 
-//skaler y aksen
+       /*vi tilføjer gitter for y-aksen*/
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (this.data.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMin() + (getMax() - getMin()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
 
+        /*og for x-aksen*/
+        for (int i = 0; i < this.data.size(); i++) {
+            if (this.data.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding) / (this.data.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if ((i % ((int) ((this.data.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+                }
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+
+        /*så opretter vi x- og y-aksen*/
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding);
+
+        Stroke oldStroke = g2.getStroke();
+        g2.setColor(lineColor);
+        g2.setStroke(GRAPH_STROKE);
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
+        }
+
+        g2.setStroke(oldStroke);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size(); i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            int ovalW = pointWidth;
+            int ovalH = pointWidth;
+            g2.fillOval(x, y, ovalW, ovalH);
+        } 
+    }
+    
+    private int getMin() {
+        int min = 0;
+        return min;
+    }
+
+    private int getMax() {
+        int max = 45;
+        return max;
     }
 
     private void simulerData() {
-      int[] simuler = new int[super.getWidth()/2]; 
-      //lav et array med halvt så mange pladser som der er pixels.
-      for (int i =0;i<simuler.length; i++)
-      {
-          //for hvert element i simuler, skal det være et tilfældig tal. 
-          
-                  }
-}
+        int[] simuler = new int[super.getWidth()/2]; 
+        //lav et array med halvt så mange pladser som der er pixels.
+        for (int i =0;i<simuler.length; i++){
+            //for hvert element i simuler, skal det være et tilfældig tal.   
+        }
+    }
 }
 
 
