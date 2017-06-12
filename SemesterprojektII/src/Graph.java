f3258d1f8b936e2f1889bb696cd01f01def3fb1c
Louise Moesgaard <louise@moesgaard.dk>
2017-06-12 12:51:04 +0200

    Graf begyndt udviklet

---
+++ SemesterprojektII/src/Graph.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+
+public class Graph extends JPanel  {
+  
+    private final int width = 800;
+    private final int heigth = 400;
+    private final Color lineColor =  new Color(44, 102, 230, 180);
+    private final Color pointColor = new Color (100, 100, 100, 180);
+    private final Color gridColor = new Color (200, 200, 200, 200);
+    private final static Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private final int pointWidth= 4;
+    private final int numberYDivisions = 9;
+    private ArrayList<Integer> data = new ArrayList<Integer>();
+    private final int padding = 25;
+    private final int labelPadding = 25;
+    
+
+
+    }
+
+
+}
